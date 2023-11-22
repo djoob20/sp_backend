3e2e7cf30afa2a78dfd6344ada6c31284ae8a722
djoob20 <djoob20@gmail.com>
2023-11-22 17:57:22 +0100

    updated gitignore

--- README.md
+++
@@ -1,3 +0,0 @@
-# sp_backend
-back end implementation of StudyPortal project
--REST API implementation using c#/.NET Cores
--- StudyPortal.API/Models/Course.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StudyPortal.API.Models;
-
-public class Course : Subjects
-{
-    public Course(string id, string title, string description, string summary, string[] authors) : base(id, title,
-        description, summary, authors)
-    {
-    }
-}
